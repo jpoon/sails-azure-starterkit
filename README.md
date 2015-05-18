249ef7016cd98d2efbd71f40162df5a96e50cf67
Jason <github@jasonpoon.ca>
2015-05-18 03:00:32 -0700

    genesis

---
+++ README.md
@@ -0,0 +1,11 @@
+# Starter Kit
+
+A [Sails](http://sailsjs.org), [Bootstrap](http://getbootstrap.com/), [AngularJS](https://angularjs.org/) starter kit prepped for hosting on [Microsoft Azure](http://azure.microsoft.com/)
+
+## Getting Started
+
+* `npm install`
+* `npm install -g nodemon` 
+* `bower install`
+* `nodemon app.js` or `nodemon app.js --prod`
+
---
+++ api/models/Task.js
@@ -0,0 +1,19 @@
+/**
+* Stacks.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    title: {
+        type: 'string',
+        primaryKey: true,
+        unique: true,
+    },
+  },
+  
+};
+
